80530c503f9f1f819e0d5cedc3300f44b3d1a167
edgarargueta <eaa7734@g.rit.edu>
2020-03-20 00:05:27 -0400

    Finished the coding problem. The current program can determine if there is a 1 to 1 mapping of the first string to the second string and prints the result. It also raises an exception if two strings are not given in the command line.

--- main.py
+++ main.py
@@ -8,10 +8,22 @@ import sys
 
 
 def main():
-    string1 = sys.argv[1]  # The first string to compare
-    string2 = sys.argv[2]  # The second string that we have to map to
-    print(string1)
-    print(string2)
+    if len(sys.argv) < 2:
+        raise Exception("Two strings are needed to run this program")
+    else:
+        string1 = sys.argv[1]  # The first string to compare
+        string2 = sys.argv[2]  # The second string that we have to map to
+        dictionary = dict()  # The map to store which character from string1 is mapped to in string2
+        condition = False  # Boolean to keep track if the string1 can be 1-to-1 mapped.
+        for i in range(len (string1)):
+            if string1[i] in dictionary:  # Check if character in dictionary
+                condition = True
+                break
+            dictionary[string1[i]] = string2[i]  # If not, map the characters
+        if condition:
+            print("false")
+        else:
+            print("true")
 
 
 if __name__ == '__main__':
